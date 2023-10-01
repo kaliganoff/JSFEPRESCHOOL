7e399d01583ad2bee4c73e6e0a6a2af2d1cdc782
kaliganoff <kaliganoff@yahoo.com>
2023-10-01 13:38:39 +0300

    refactor: Make better design

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -9,21 +9,32 @@ if (localStorage.query) {
   getImages(query);
 }
 
+search.focus();
+search.select();
+search.placeholder = query;
+
 async function getImages(query) {
     const res = await fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=30&client_id=F-CrqZ66gvClK9UaCKcUu4dEWfydP2o8-5M2Uzt0rds`);
     const data = await res.json();
-    console.log(data.results);
     for (let i = 0; i < 30; i++) {
+      const div = document.createElement('div');
+      div.classList.add('gallery-img-container')
+      container.append(div);
       const img = document.createElement('img');
       img.classList.add('gallery-img')
       img.src = `${data.results[i].urls.regular}`;
       img.alt = `image`;
-      container.append(img);
+      div.append(img);
+      img.addEventListener('click', () =>{
+        window.open(img.src);
+      })
   }
   }
 
   search.addEventListener('search', () => {
-    container.innerHTML = ';'
-    localStorage.query = search.value;
-   getImages(search.value);
+    if (search.value) {
+      container.innerHTML = '';
+      localStorage.query = search.value;
+      getImages(search.value);
+    }
   })
